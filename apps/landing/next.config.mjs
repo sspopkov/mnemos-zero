d3bd6d6a649b9fea04e8a4103fef5b2b41cb43a2
Sergey Popkov <sspopkov@list.ru>
2026-03-24 00:36:05 +0300

    chore: setup ci/cd and docker configuration

--- apps/landing/next.config.mjs
+++ apps/landing/next.config.mjs
@@ -1,7 +1,6 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-// Эмуляция __dirname в ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
